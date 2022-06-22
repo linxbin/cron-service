31f252264ce986d4b801bebb998f14c424491b15
linxb <448923515@qq.com>
2022-06-22 16:06:45 +0800

    Init Project

---
+++ internal/model/model.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	"github.com/linxbin/corn-service/global"
+	"github.com/linxbin/corn-service/pkg/setting"
+)
+
+func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+	s := "%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local"
+	db, err := gorm.Open(databaseSetting.DBType, fmt.Sprintf(s,
+		databaseSetting.UserName,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParseTime,
+	))
+	if err != nil {
+		return nil, err
+	}
+
+	if global.ServerSetting.RunMode == "debug" {
+		db.LogMode(true)
+	}
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(databaseSetting.MaxIdleConns)
+	db.DB().SetMaxOpenConns(databaseSetting.MaxOpenConns)
+
+	return db, nil
+}
